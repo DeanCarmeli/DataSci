c97450de76b3d88a9701c046c2f94f4b4075a573
Elad <eladb3@mail.tau.ac.il>
2020-02-25 19:05:15 +0200

    notebook-update

--- run_model/regression_models.py
+++ run_model/regression_models.py
@@ -3,7 +3,12 @@ import pandas as pd
 import statsmodels.api as sm
 
 
-
+def predict_linear_reg(model, data, y_col):
+    model = run_linear_reg(data, print_summary = False, print_r2 = True, y_col = y_col)
+    data = sm.add_constant(data, prepend=False)
+    X = data.drop(y_col, axis = 1)
+    Y = data[y_col]
+    return model.predict(X)
 
 def run_linear_reg(data, print_summary = False, print_r2 = True, y_col = "aar_5"):
     """
