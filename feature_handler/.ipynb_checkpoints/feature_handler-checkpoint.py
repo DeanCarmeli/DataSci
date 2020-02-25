0bd12b7241b47c4a471fa2d2b7a0ac859c373d16
Elad <eladb3@mail.tau.ac.il>
2020-02-25 10:36:52 +0200

    update

---
+++ feature_handler/.ipynb_checkpoints/feature_handler-checkpoint.py
@@ -0,0 +1,132 @@
+import pandas as pd
+
+
+def create_div_amount_num(df):
+    """
+    Detects invalid values of dividend_amount and converts string to number.
+    Saves it as a new feature
+    :param df: DataFrame
+    :return: DataFrame with a column named "div_amount_num"
+    """
+    drop = 0
+    print("################# Creating div_amount_num #################")
+    # Find rows were dividend_amount doesn't start with $
+    valid = df["dividend_amount"].str.startswith('$').sum()
+    print("Found {} samples were dividend_amount did not start with $ - drop".format(df.shape[0]-valid))
+    drop += df.shape[0]-valid
+    df.drop(df[~df["dividend_amount"].str.startswith('$')].index, inplace=True)
+
+    # Create a new column with the dividend amount as float
+    df["div_amount_num"] = df["dividend_amount"].apply(lambda x: float(x.split("$")[1]))
+    row_before = df.shape[0]
+
+    # Drop dividens that equal to 0
+    df.drop(df[df["div_amount_num"] == 0].index, inplace=True)
+    print("Found {} divdends that were eqaul to 0 - drop".format(row_before - df.shape[0]))
+    drop += row_before - df.shape[0]
+    print(">>> Finished. Total dropped rows due to invalid dividend info: {}".format(drop))
+    return df
+
+
+def gen_div_direction_and_change(df):
+    """
+    Adds 2 features:
+    div_direction = whether the dividend went up (1), down (-1)
+    or flat (0) vs. last announcement.
+    div_change = By how much the dividend change in %
+    :param df: DataFrame
+    :return: DataFrame
+    """
+    print("################# Creating div_direction and div_change ###")
+    df.reset_index(inplace=True, drop=True)
+    df["temp"] = df["company_name"] + "___" + df["div_amount_num"].apply(lambda x: str(x))
+    direction = []
+    change = []
+    for index, value in df["temp"].iteritems():
+        try:
+            prev = df["temp"].iat[index + 1]
+        except Exception:
+            direction.append(1)
+            change.append(1)
+            continue
+        curr_info = value.split("___")
+        prev_info = prev.split("___")
+        if curr_info[0] == prev_info[0]:
+            change.append((float(curr_info[1]) - float(prev_info[1])) / float(prev_info[1]))
+            if float(curr_info[1]) == float(prev_info[1]):
+                direction.append(0)
+                continue
+            if float(curr_info[1]) < float(prev_info[1]):
+                direction.append(-1)
+                continue
+        else:
+            change.append(1)
+        direction.append(1)
+    df["div_direction"] = direction
+    df["div_change"] = change
+    df.drop(["temp"], axis=1, inplace=True)
+    print(">>> Finished")
+    return df
+
+
+def create_abnormal_return(df):
+    """
+    Adds the following features:
+    1. expected_t = the calculated expected price at t days from announcement date
+                    expected_t = alpha + beta * sp_price_t
+    2. ar_t = the abnormal return at day t from announcement date
+                    ar_t = price_t - expected_t
+    3. aar_x = the average abnormal return over a time window of x days before
+               and after the announcement date:
+                    aar_x = average(aar_-x, ...., aar_x)
+    4. aar_x% = aar_x divided by the average of expected prices during
+                the time window
+                    aar_x% = aar_x/average(expected_t-x, ..., expected_tx)
+    :param df: DataFrame
+    :return: DataFrame with the extra features
+    """
+    days = [x for x in range(-5, 6)]
+    expected_cols = ["expected_t{}".format(x) for x in days]
+    ar_cols = ["ar_t{}".format(x) for x in days]
+    aar_cols = ["aar_{}".format(x) for x in range(0, 6)]
+    aar_percen_cols = ["aar_{}%".format(x) for x in range(0, 6)]
+    temp_cols = ["temp_{}".format(x) for x in range(0, 6)]
+    print("################# Creating abnormal return related features ###")
+    print("\tfeature type = expected_t\t\t11 features")
+    for i in range(len(expected_cols)):
+        df[expected_cols[i]] = df["alpha"].add(df["beta"].mul(df["sp_price_t{}".format(str(days[i]))]))
+
+    print("\tfeature type = ar_t\t\t11 features")
+    for i in range(len(ar_cols)):
+        df[ar_cols[i]] = df["price_t{}".format(str(days[i]))].sub(df[expected_cols[i]])
+
+    print("\tfeature type = aar_t\t\t6 features")
+    df["temp_0"] = df["ar_t0"]
+    for i in range(1, len(temp_cols)):
+        df[temp_cols[i]] = df["ar_t-{}".format(i)].add(df["temp_{}".format(i - 1)].add(df["ar_t{}".format(i)]))
+    for i in range(len(aar_cols)):
+        if i == 0:
+            df[aar_cols[i]] = df[temp_cols[i]]
+        else:
+            df[aar_cols[i]] = df[temp_cols[i]].div(i)
+
+    print("\tfeature type = aar_t%\t\t6 features")
+    df["temp_0"] = df["expected_t0"]
+    for i in range(1, len(temp_cols)):
+        df[temp_cols[i]] = df["expected_t-{}".format(i)].add(
+            df["temp_{}".format(i - 1)].add(df["expected_t{}".format(i)]))
+    for i in range(len(aar_percen_cols)):
+        if i == 0:
+            df[aar_percen_cols[i]] = df["aar_{}".format(i)].div(df["temp_{}".format(i)])
+        else:
+            df[aar_percen_cols[i]] = df["aar_{}".format(i)].div(df["temp_{}".format(i)].div(i))
+    df.drop(temp_cols, axis=1, inplace=True)
+    print(">>> Finished. Created {} features".format(34))
+    return df
+
+
+def gen_delta_precent_t(df, print_ = True):
+    df['delta_%_t-5'] = (df['price_t-5'] - df['expected_t-5']) / df['expected_t-5']
+    df['delta_%_t-4'] = (df['price_t-4'] - df['expected_t-4']) / df['expected_t-4']
+    if print_: print("Created delta_%_t-4 and delta_%_t-5 features")
+    return df
---
+++ run_model/.ipynb_checkpoints/regression_models-checkpoint.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+
+
+def split_test_train(df, y):
+    pass
+
+def generate_bl_model_data(df, print_ = True, y_col = "aar_5"):
+    """
+    Generate data for the baseline models in step 3.
+    """
+    cat_cols = list(df.columns[df.dtypes == 'object'])
+    cat_cols.remove('sector')
+    drop_cols_baseline = ['price_t-5', 'vol_t-5', 'sp_price_t-5', 'sp_vol_t-5',
+           'price_t-4', 'vol_t-4', 'sp_price_t-4', 'sp_vol_t-4', 'price_t-3',
+           'vol_t-3', 'sp_price_t-3', 'sp_vol_t-3', 'price_t-2', 'vol_t-2',
+           'sp_price_t-2', 'sp_vol_t-2', 'price_t-1', 'vol_t-1', 'sp_price_t-1',
+           'sp_vol_t-1', 'price_t0', 'vol_t0', 'sp_price_t0', 'sp_vol_t0',
+           'price_t1', 'vol_t1', 'sp_price_t1', 'sp_vol_t1', 'price_t2', 'vol_t2',
+           'sp_price_t2', 'sp_vol_t2', 'price_t3', 'vol_t3', 'sp_price_t3',
+           'sp_vol_t3', 'price_t4', 'vol_t4', 'sp_price_t4', 'sp_vol_t4',
+           'price_t5', 'vol_t5', 'sp_price_t5', 'sp_vol_t5','expected_t-5', 'expected_t-4', 'expected_t-3',
+           'expected_t-2', 'expected_t-1', 'expected_t0', 'expected_t1',
+           'expected_t2', 'expected_t3', 'expected_t4', 'expected_t5', 'ar_t-5',
+           'ar_t-4', 'ar_t-3', 'ar_t-2', 'ar_t-1', 'ar_t0', 'ar_t1', 'ar_t2',
+           'ar_t3', 'ar_t4', 'ar_t5', 'aar_0', 'aar_1', 'aar_2', 'aar_3', 'aar_4', 'aar_5',
+           'aar_0%', 'aar_1%', 'aar_2%', 'aar_3%', 'aar_4%', 'aar_5%'] + cat_cols
+    drop_cols_baseline.remove(y_col)
+    baseline_models_data = df.drop(drop_cols_baseline, axis = 1)
+    baseline_models_data = pd.get_dummies(data = baseline_models_data,\
+                                          prefix='sector',
+                                          columns = ['sector'],
+                                          drop_first = True)
+    baseline_models_data.reset_index(inplace = True, drop = True)
+    if print_: print("Baseline model features: {}".format(list(baseline_models_data.columns)))
+    return baseline_models_data
+
+def run_linear_reg_research(data , print_summary = False, print_r2 = True):
+    """
+    Run linear regression only on samples with div_change!=0
+    param: data: DataFrame , print_:Boolean
+    return: linear regression model
+    """
+    data = data[data['div_direction']!=0]
+    return run_linear_reg(data, print_summary = print_summary, print_r2 = print_r2, y_col = "aar_5")
+
+def run_linear_reg(data, print_summary = False, print_r2 = True, y_col = "aar_5"):
+    """
+    Run linear regression
+    param: data: DataFrame , print_:Boolean
+    return: linear regression model
+    """
+    data = sm.add_constant(data, prepend=False)
+    X = data.drop(y_col, axis = 1)
+    Y = data[y_col]
+    result = sm.OLS(Y, X).fit()
+    if print_summary: print(result.summary())
+    if not print_summary and print_r2: print('R^2: {}'.format(result.rsquared))
+    return result
